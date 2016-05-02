0e598b9e8e3f974ba5a7a8c233ead92ce0c368dc
Loyal Chow <loyalchow@Loyals-MacBook-Pro.local>
2016-05-02 15:08:30 -0700

    fix syntax error in test

--- tests/scripts/test.js
+++ tests/scripts/test.js
@@ -2,7 +2,7 @@ module.exports = {
   'Demo Test': function(browser) {
     browser
       .url('http://ci.server:8080')
-      .execute('/*@screener.snapshot*/', ['Home']);
+      .execute('/*@screener.snapshot*/', ['Home'])
       .end();
   }
 };
